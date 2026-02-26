36e8b50b25a9cd5967c9be8ac95b0c851ebf2164
HirotoMatsui <236494398+ZN86TRD@users.noreply.github.com>
2026-02-26 09:17:39 +0900

    2/26学習分

---
+++ src/Car4.java
@@ -0,0 +1,21 @@
+
+public class Car4 {
+	private int no;
+	private static int count;
+	
+	public Car4() {
+		no = 0;
+		count++;
+		System.out.println("ナンバーなしを作りました");
+	}
+	
+	public Car4(int n) {
+		no = n;
+		count++;
+		System.out.println("ナンバー" + no + "を作りました");
+	}
+	
+	public void display() {
+		System.out.println(count + "台作成済みです");
+	}
+}
