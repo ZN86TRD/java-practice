692980d74372cc9470f83120a6e8898ecd85db83
HirotoMatsui <236494398+ZN86TRD@users.noreply.github.com>
2026-03-09 22:06:49 +0900

    3/9学習分

--- src/Person5.java
+++ src/Person5.java
@@ -1,4 +1,12 @@
 
 public class Person5 {
-
+	private String name;
+	
+	public Person5(String name) {
+		this.name = name;
+	}
+	
+	public void display() {
+		System.out.println("名前：" + name);
+	}
 }
