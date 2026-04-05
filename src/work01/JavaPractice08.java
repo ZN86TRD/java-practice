1786358a539b5701be8c17378434495a7995383b
HirotoMatsui <236494398+ZN86TRD@users.noreply.github.com>
2026-04-05 18:34:44 +0900

    4/5学習分

--- src/work01/JavaPractice08.java
+++ src/work01/JavaPractice08.java
@@ -30,8 +30,58 @@ public class JavaPractice08 {
 			if(menu == 1) {
 				manager.showAllProducts();
 			}else if(menu == 2) {
+				System.out.print("検索する商品コードを入力してください：");
+				String code = scanner.nextLine();
 				
+				SearchResult03 result = manager.findProductByCode(code);
+				
+				if(result.isFound()) {
+					System.out.println("商品が見つかりました");
+					result.getProduct().showInfo();
+				} else {
+					System.out.println("商品が見つかりませんでした");
+				}
+			} else if(menu == 3) {
+				System.out.println("入庫する商品コードを入力してください：");
+				String code = scanner.nextLine();
+				
+				System.out.println("入庫数を入力してください：");
+				int quantity = scanner.nextInt();
+				scanner.nextLine();
+				
+				boolean success = manager.stockIn(code, quantity);
+				
+				if(success) {
+					System.out.println("入庫しました。");
+				} else {
+					System.out.println("商品コードが見つかりませんでした。");
+				}
+			} else if(menu == 4) {
+				System.out.println("出庫する商品コードを入力してください：");
+				String code = scanner.nextLine();
+				
+				System.out.println("出庫数を入力してください：");
+				int quantity = scanner.nextInt();
+				scanner.nextLine();
+				
+				int result = manager.stockOut(code, quantity);
+				
+				if(result == 1) {
+					System.out.println("出庫しました。");
+				} else if(result == -1) {
+					System.out.println("在庫が足りません。");
+				} else {
+					System.out.println("商品コードが見つかりませんでした。");
+				}
+			} else if(menu == 5) {
+				manager.showLowStockProducts();
+			} else if(menu == 0) {
+				running = false;
+				System.out.println("終了します。");
+			}else {
+				System.out.println("正しい番号を入力してください。");
 			}
 		}
+		scanner.close();
 	}
 }
--- src/work01/ProductManager03.java
+++ src/work01/ProductManager03.java
@@ -1,7 +1,5 @@
 package work01;
 
-import javax.naming.directory.SearchResult;
-
 public class ProductManager03 {
 	private Product03[]products;
 	private int count;
@@ -34,7 +32,57 @@ public class ProductManager03 {
 		}
 	}
 	
-	public SearchResult findProductByCode(String code) {
-		//  4/2ここまで
+	public SearchResult03 findProductByCode(String code) {
+		for(int i = 0; i < count; i++) {
+			if(products[i].getCode().equals(code)) {
+				return new SearchResult03(true, products[i]);
+			}
+		}
+		return new SearchResult03(false, null);
+	}
+	
+	public boolean stockIn(String code, int quantity) {
+		SearchResult03 result = findProductByCode(code);
+		
+		if(result.isFound()) {
+			result.getProduct().addStock(quantity);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public int stockOut(String code, int quantity) {
+		SearchResult03 result = findProductByCode(code);
+		
+		if(!result.isFound()) {
+			return 0;
+		}
+		
+		boolean success = result.getProduct().removeStock(quantity);
+		
+		if(success) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+	
+	public void showLowStockProducts() {
+		boolean found = false;
+		
+		System.out.println("---- 在庫少なめ商品 ----");
+		
+		for(int i = 0; i < count; i++) {
+			if(products[i].isLowStock()) {
+				products[i].showInfo();
+				System.out.println("------------------------");
+				found = true;
+			}
+		}
+		
+		if(!found) {
+			System.out.println("在庫少なめの商品はありません");
+		}
 	}
 }
